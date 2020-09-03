22cc66612dc5b558cc2cc6449680e8e1f2f19640
Lee-Ann Clark <leeann74c@gmail.com>
2020-09-03 17:25:50 +0100

    card selection seperate page

---
+++ assets/js/cards.js
@@ -0,0 +1,293 @@
+//card options
+const easycards = [{
+    name: "clown-fish",
+    img: "/assets/images/clown-fish.png",
+},
+{
+    name: "clown-fish",
+    img: "/assets/images/clown-fish.png",
+},
+{
+    name: "coral",
+    img: "/assets/images/coral.png",
+},
+{
+    name: "coral",
+    img: "/assets/images/coral.png",
+},
+{
+    name: "dolphin",
+    img: "/assets/images/dolphin.png",
+},
+{
+    name: "dolphin",
+    img: "/assets/images/dolphin.png",
+},
+{
+    name: "shark",
+    img: "/assets/images/shark.png",
+},
+{
+    name: "shark",
+    img: "/assets/images/shark.png",
+},
+{
+    name: "stingrays",
+    img: "/assets/images/stingrays.png",
+},
+{
+    name: "stingrays",
+    img: "/assets/images/stingrays.png",
+},
+{
+    name: "turtle",
+    img: "/assets/images/turtle.png",
+},
+{
+    name: "turtle",
+    img: "/assets/images/turtle.png",
+},
+{
+    name: "lionfish",
+    img: "/assets/images/lionfish.png",
+},
+{
+    name: "lionfish",
+    img: "/assets/images/lionfish.png",
+},
+{
+    name: "seal",
+    img: "/assets/images/seal.png",
+},
+{
+    name: "seal",
+    img: "/assets/images/seal.png",
+},
+{
+    name: "starfish",
+    img: "/assets/images/starfish.png",
+},
+{
+    name: "starfish",
+    img: "/assets/images/starfish.png",
+},
+];
+
+const mediumcards = [{
+    name: 'clown-fish',
+    img: '/assets/images/clown-fish.png'
+},
+{
+    name: 'clown-fish',
+    img: '/assets/images/clown-fish.png'
+},
+{
+    name: 'coral',
+    img: '/assets/images/coral.png'
+},
+{
+    name: 'coral',
+    img: '/assets/images/coral.png'
+},
+{
+    name: 'dolphin',
+    img: '/assets/images/dolphin.png'
+},
+{
+    name: 'dolphin',
+    img: '/assets/images/dolphin.png'
+},
+{
+    name: 'shark',
+    img: '/assets/images/shark.png'
+},
+{
+    name: 'shark',
+    img: '/assets/images/shark.png'
+},
+{
+    name: 'stingrays',
+    img: '/assets/images/stingrays.png'
+},
+{
+    name: 'stingrays',
+    img: '/assets/images/stingrays.png'
+},
+{
+    name: 'turtle',
+    img: '/assets/images/turtle.png'
+},
+{
+    name: 'turtle',
+    img: '/assets/images/turtle.png'
+},
+{
+    name: 'lionfish',
+    img: '/assets/images/lionfish.png'
+},
+{
+    name: 'lionfish',
+    img: '/assets/images/lionfish.png'
+},
+{
+    name: 'seal',
+    img: '/assets/images/seal.png'
+},
+{
+    name: 'seal',
+    img: '/assets/images/seal.png'
+},
+{
+    name: 'starfish',
+    img: '/assets/images/starfish.png'
+},
+{
+    name: 'starfish',
+    img: '/assets/images/starfish.png'
+},
+{
+    name: 'jellyfish',
+    img: '/assets/images/jellyfish.png'
+},
+{
+    name: 'jellyfish',
+    img: '/assets/images/jellyfish.png'
+},
+{
+    name: 'seahorse',
+    img: '/assets/images/seahorse.png'
+},
+{
+    name: 'seahorse',
+    img: '/assets/images/seahorse.png'
+},
+{
+    name: 'eel',
+    img: '/assets/images/eel.png'
+},
+{
+    name: 'eel',
+    img: '/assets/images/eel.png'
+},
+];
+const hardcards = [{
+    name: 'clown-fish',
+    img: '/assets/images/clown-fish.png'
+},
+{
+    name: 'clown-fish',
+    img: '/assets/images/clown-fish.png'
+},
+{
+    name: 'coral',
+    img: '/assets/images/coral.png'
+},
+{
+    name: 'coral',
+    img: '/assets/images/coral.png'
+},
+{
+    name: 'dolphin',
+    img: '/assets/images/dolphin.png'
+},
+{
+    name: 'dolphin',
+    img: '/assets/images/dolphin.png'
+},
+{
+    name: 'shark',
+    img: '/assets/images/shark.png'
+},
+{
+    name: 'shark',
+    img: '/assets/images/shark.png'
+},
+{
+    name: 'stingrays',
+    img: '/assets/images/stingrays.png'
+},
+{
+    name: 'stingrays',
+    img: '/assets/images/stingrays.png'
+},
+{
+    name: 'turtle',
+    img: '/assets/images/turtle.png'
+},
+{
+    name: 'turtle',
+    img: '/assets/images/turtle.png'
+},
+{
+    name: 'lionfish',
+    img: '/assets/images/lionfish.png'
+},
+{
+    name: 'lionfish',
+    img: '/assets/images/lionfish.png'
+},
+{
+    name: 'seal',
+    img: '/assets/images/seal.png'
+},
+{
+    name: 'seal',
+    img: '/assets/images/seal.png'
+},
+{
+    name: 'starfish',
+    img: '/assets/images/starfish.png'
+},
+{
+    name: 'starfish',
+    img: '/assets/images/starfish.png'
+},
+{
+    name: 'jellyfish',
+    img: '/assets/images/jellyfish.png'
+},
+{
+    name: 'jellyfish',
+    img: '/assets/images/jellyfish.png'
+},
+{
+    name: 'urchin',
+    img: '/assets/images/urchin.png'
+},
+{
+    name: 'urchin',
+    img: '/assets/images/urchin.png'
+},
+{
+    name: 'seahorse',
+    img: '/assets/images/seahorse.png'
+},
+{
+    name: 'seahorse',
+    img: '/assets/images/seahorse.png'
+},
+{
+    name: 'eel',
+    img: '/assets/images/eel.png'
+},
+{
+    name: 'eel',
+    img: '/assets/images/eel.png'
+},
+{
+    name: 'octopus',
+    img: '/assets/images/octopus.png'
+},
+{
+    name: 'octopus',
+    img: '/assets/images/octopus.png'
+},
+{
+    name: 'manatee',
+    img: '/assets/images/manatee.png'
+},
+{
+    name: 'manatee',
+    img: '/assets/images/manatee.png'
+},
+];
---
+++ assets/js/script.js
@@ -0,0 +1,141 @@
+document.addEventListener("DOMContentLoaded", () => {
+     
+    /* Sound Control */
+
+class AudioController {
+    constructor() {
+        this.bgMusic = Audio('assets/audio/background-audio.mp3');
+        this.flipSound = Audio('assets/audio-card-flip.mp3');
+        this.winningSound = Audio('assets/audio/winning.mp3');
+    }
+    startMusic() {
+        this.bgMusic.play();
+    }
+    stopMusic() {
+        this.bgMusic.pause();
+    }
+    flip() {
+        this.flipSound.play();
+    }
+    winning() {
+        this.stopMusic();
+        this.winningSound.play();
+    }
+}
+function muteIcon() {
+    if (document.getElementById("muteId").classList.contains('fa-volume-mute')) {
+        document.getElementById("muteId").classList.toggle('fa-volume-up');
+    }
+}
+    
+    const gameDifficulty = {
+        easy: easycards,
+        medium: mediumcards,
+        hard: hardcards,
+    };
+
+        function setDifficulty(e) {
+        difficultyLevel = e.target.id;
+        cardArray = gameDifficulty[difficultyLevel];
+        cardArray.sort(() => 0.5 - Math.random());
+        createBoard();
+    }
+
+    
+    let difficultyLevel = "easy";
+    let cardArray = gameDifficulty[difficultyLevel];
+    const easyButton = document.querySelector("#easy");
+    const mediumButton = document.querySelector("#medium");
+    const hardButton = document.querySelector("#hard");
+    easyButton.addEventListener("click", setDifficulty);
+    mediumButton.addEventListener("click", setDifficulty);
+    hardButton.addEventListener("click", setDifficulty);
+
+    cardArray.sort(() => 0.5 - Math.random());
+
+    const grid = document.querySelector(".grid");
+    const resultDisplay = document.querySelector("#result");
+    var cardsChosen = [];
+    var cardsChosenId = [];
+    const cardsWon = [];
+
+    //create your board
+    function createBoard() {
+        grid.innerHTML = "";
+        for (let i = 0; i < cardArray.length; i++) {
+            var card = document.createElement("img");
+            card.setAttribute("src", "/assets/images/blank.png");
+            card.setAttribute("data-id", i);
+            card.addEventListener("click", flipCard);
+            grid.appendChild(card);
+        }
+    }
+
+    //check for matches
+    function checkForMatch() {
+        var cards = document.querySelectorAll("img");
+        const optionOneId = cardsChosenId[0];
+        const optionTwoId = cardsChosenId[1];
+
+        if (optionOneId == optionTwoId) {
+            cards[optionOneId].setAttribute("src", "/assets/images/blank.png");
+            cards[optionTwoId].setAttribute("src", "/assets/images/blank.png");
+        } else if (cardsChosen[0] === cardsChosen[1]) {
+            cards[optionOneId].setAttribute("src", "/assets/images/white.png");
+            cards[optionTwoId].setAttribute("src", "/assets/images/white.png");
+            cards[optionOneId].removeEventListener("click", flipCard);
+            cards[optionTwoId].removeEventListener("click", flipCard);
+            cardsWon.push(cardsChosen);
+        } else {
+            cards[optionOneId].setAttribute("src", "/assets/images/blank.png");
+            cards[optionTwoId].setAttribute("src", "/assets/images/blank.png");
+        }
+        cardsChosen = [];
+        cardsChosenId = [];
+        resultDisplay.textContent = cardsWon.length;
+        if (cardsWon.length === cardArray.length / 2) {
+            resultDisplay.textContent = " You found them all!";
+        }
+    }
+
+    //flip your card
+    function flipCard() {
+        var cardId = this.getAttribute("data-id");
+        cardsChosen.push(cardArray[cardId].name);
+        cardsChosenId.push(cardId);
+        this.setAttribute("src", cardArray[cardId].img);
+        if (cardsChosen.length === 2) {
+            setTimeout(checkForMatch, 500);
+        }
+    }
+
+    createBoard();
+
+    
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal
+    btn.onclick = function () {
+        modal.style.display = "block";
+    };
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    };
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+    }
+    
+    };
+});
