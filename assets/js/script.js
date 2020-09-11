af5c3ae5438122e9033ec04c92ca7b88e3ed7102
Lee-Ann Clark <leeann74c@gmail.com>
2020-09-11 18:42:56 +0100

    sound added and button fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,14 @@
 //While coding this I followed a tutorial to achieve basic game functionality, then built my own features on top of that code, see credits in ReadME
 
-document.addEventListener("DOMContentLoaded", () => {
-
-    /* Sound Control */
-
+/* Sound Control */
 class AudioController {
     constructor() {
         this.bgMusic = new Audio('assets/audio/bgmusic.mp3');
         this.flipSound = new Audio('assets/audio/card-flip.mp3');
         this.flipSound.volume = 0.3;
-        this.bgMusic.volume = 0.8;
+        this.bgMusic.volume = 0.2;
         this.bgMusic.loop = true; // so that if player is between levels the audio does not stop
-        /*this.audioController.startMusic();*/
+        
     }
     startMusic() {
         this.bgMusic.play();
@@ -19,39 +16,24 @@ class AudioController {
     stopMusic() {
         this.bgMusic.pause();
     }
-
     flip() {
         this.flipSound.play();
     }
-   
 }
-
-function play() {
-    this.audioController.playMusic();
-   
-}
-
-function muteIcon() {
-    if (document.getElementById("muteId").classList.contains('fa-volume-mute')) {
-        document.getElementById("muteId").classList.toggle('fa-volume-up');
-    }
-}
-   
-    
+document.addEventListener("DOMContentLoaded", () => {
+    let audioController = new AudioController();
+    audioController.startMusic();
     const gameDifficulty = {
         easy: easycards,
         medium: mediumcards,
         hard: hardcards,
     };
-
-        function setDifficulty(e) {
+    function setDifficulty(e) {
         difficultyLevel = e.target.id;
         cardArray = gameDifficulty[difficultyLevel];
         cardArray.sort(() => 0.5 - Math.random());
         createBoard();
     }
-
-    
     let difficultyLevel = "easy";
     let cardArray = gameDifficulty[difficultyLevel];
     const easyButton = document.querySelector("#easy");
@@ -60,15 +42,12 @@ function muteIcon() {
     easyButton.addEventListener("click", setDifficulty);
     mediumButton.addEventListener("click", setDifficulty);
     hardButton.addEventListener("click", setDifficulty);
-
     cardArray.sort(() => 0.5 - Math.random());
-
     const grid = document.querySelector(".grid");
     const resultDisplay = document.querySelector("#result");
     var cardsChosen = [];
     var cardsChosenId = [];
     const cardsWon = [];
-
     //create your board
     function createBoard() {
         grid.innerHTML = "";
@@ -80,13 +59,11 @@ function muteIcon() {
             grid.appendChild(card);
         }
     }
-
     //check for matches
     function checkForMatch() {
         var cards = document.querySelectorAll("img");
         const optionOneId = cardsChosenId[0];
         const optionTwoId = cardsChosenId[1];
-
         if (optionOneId == optionTwoId) {
             cards[optionOneId].setAttribute("src", "assets/images/blank.png");
             cards[optionTwoId].setAttribute("src", "assets/images/blank.png");
@@ -107,7 +84,6 @@ function muteIcon() {
             resultDisplay.textContent = " You Won!";
         }
     }
-
     //flip your card
     function flipCard() {
         var cardId = this.getAttribute("data-id");
@@ -118,50 +94,39 @@ function muteIcon() {
             setTimeout(checkForMatch, 500);
         }
     }
-
     createBoard();
-
-    
     // Get the modal
     var modal = document.getElementById("myModal");
-
     // Get the button that opens the modal
     var btn = document.getElementById("myBtn");
-
     // Get the <span> element that closes the modal
     var span = document.getElementsByClassName("close")[0];
-
     // When the user clicks on the button, open the modal
     btn.onclick = function () {
         modal.style.display = "block";
     };
-
     // When the user clicks on <span> (x), close the modal
     span.onclick = function () {
         modal.style.display = "none";
     };
-
+    // volume button
+    document.getElementById('muteButton').addEventListener("click", () => {
+        let mute_btn = document.getElementById("muteId");
+        console.log("clicked sound button");
+        if (mute_btn.classList.contains('fa-volume-up')) {
+            console.log("found fa-volume-up, going to mute");
+            audioController.stopMusic();
+        } else {
+            console.log("found fa-volume-up, going to play that music");
+            audioController.startMusic();
+        }
+        mute_btn.classList.toggle('fa-volume-up');
+        mute_btn.classList.toggle('fa-volume-mute');
+    });
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function (event) {
         if (event.target == modal) {
             modal.style.display = "none";
-    }
-    
-     let muteButton = document.getElementById('muteButton').addEventListener("click", () => {
-        if (document.getElementById("muteId").classList.contains('fa-volume-up')) {
-            game.mute();
-        } else {
-            game.unmute();
         }
-    });
-    function mute() {
-        this.audioController.stopMusic();
-    }
-
-    function unmute() {
-        this.audioController.startMusic();
-         
-    }
-     
     };
 });
